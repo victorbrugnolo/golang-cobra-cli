bdcd43957757af1fea56a9ae78b899bbac772625
victorbrugnolo <brugnolovictor@gmail.com>
2024-04-16 19:41:50 -0300

    create command with parameters

--- cmd/test.go
+++ cmd/test.go
@@ -1,18 +1,15 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
-// pingCmd represents the ping command
-var pingCmd = &cobra.Command{
-	Use:   "ping",
+// testCmd represents the test command
+var testCmd = &cobra.Command{
+	Use:   "test",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +18,28 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ping called")
+		command, _ := cmd.Flags().GetString("command")
+
+		if command == "ping" {
+			cmd.Println("ping")
+		} else {
+			cmd.Println("pong")
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(pingCmd)
+	rootCmd.AddCommand(testCmd)
+	testCmd.Flags().StringP("command", "c", "", "Choose ping or pong")
+	testCmd.MarkFlagRequired("command")
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// pingCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// testCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// pingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// testCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
