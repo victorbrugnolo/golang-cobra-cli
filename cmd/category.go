239f7c16d9ddfc06e0e39c5dffb3b8b70df5b2c7
victorbrugnolo <brugnolovictor@gmail.com>
2024-04-16 19:54:15 -0300

    create persistent flag

--- cmd/category.go
+++ cmd/category.go
@@ -24,6 +24,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(categoryCmd)
+	categoryCmd.PersistentFlags().String("name", "", "Name of the category")
 
 	// Here you will define your flags and configuration settings.
 
