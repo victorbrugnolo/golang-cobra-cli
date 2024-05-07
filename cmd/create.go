8e4b5b7bc99419429330a79ea2d68f53361c8948
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-07 14:31:56 -0300

    save category on db

--- cmd/create.go
+++ cmd/create.go
@@ -1,40 +1,43 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/victorbrugnolo/golang-cobra-cli/internal/database"
 )
 
-// createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
-	},
+func newCreateCmd(categoryDB database.Category) *cobra.Command {
+	return &cobra.Command{
+		Use:   "create",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
+	and usage of using your command. For example:
+	
+	Cobra is a CLI library for Go that empowers applications.
+	This application is a tool to generate the needed files
+	to quickly create a Cobra application.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			name, _ := cmd.Flags().GetString("name")
+			description, _ := cmd.Flags().GetString("description")
+			_, _ = categoryDB.Create(name, description)
+		},
+	}
 }
 
+//func runCreate(categoryDB database.Category) RunEFunc {
+//	return func(cmd *cobra.Command, args []string) error {
+//		name, _ := cmd.Flags().GetString("name")
+//		description, _ := cmd.Flags().GetString("description")
+//		_, _ = categoryDB.Create(name, description)
+//	}
+//}
+
 func init() {
+	createCmd := newCreateCmd(GetCategoryDB(GetDB()))
 	categoryCmd.AddCommand(createCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	createCmd.Flags().StringP("name", "n", "", "Name of the category")
+	createCmd.Flags().StringP("description", "d", "", "Description of the category")
+	createCmd.MarkFlagsRequiredTogether("name", "description")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,17 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"database/sql"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/victorbrugnolo/golang-cobra-cli/internal/database"
+
+	_ "github.com/mattn/go-sqlite3"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,6 +26,22 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+type RunEFunc func(cmd *cobra.Command, args []string) error
+
+func GetDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func GetCategoryDB(db *sql.DB) database.Category {
+	return *database.NewCategory(db)
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +64,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
